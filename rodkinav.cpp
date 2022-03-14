1b0f74443dbf0a88d8c63c7edaf1f14368173754
Stavlenik <79925219+Stavlenik@users.noreply.github.com>
2022-03-14 08:04:53 +0300

    Update rodkinav.cpp

--- rodkinav.cpp
+++ rodkinav.cpp
@@ -14,36 +14,61 @@ void rodkinav::lab1()
  */
 void rodkinav::lab2()
 {
-    double Q = 0;
+	int max;
+	double sum = 0;
+	//прямой ход
+	for (int i = 0; i < N; i++)
+	{
+		max = i;
+		double* vspom; double vspom_1;
 
-    for (int i = 0; i < N - 1; i++)
-    {
-        for (int j = i + 1; j < N; j++)
-        {
-            Q = A[j][i] / A[i][i];
+		for (int j = i + 1; j < N; j++)
+		{
+			if (fabs(A[j][i]) > fabs(A[max][i]))
+			{
+				max = j;
+			}
+		}
+		if (max != i)
+		{
+			vspom = A[i];
+			A[i] = A[max];
+			A[max] = vspom;
 
-            for (int k = i; k < N; k++)
-            {
-                A[j][k] -= Q * A[i][k];
-            }
-            b[j] -= Q * b[i];
-        }
-    }
+			vspom_1 = b[i];
+			b[i] = b[max];
+			b[max] = vspom_1;
+		}
 
-    for (int i = 0; i < N; i++)
-    {
-        x[i] = b[i];
-    }
+		for (int j = N - 1; j > i; j--)
+		{
+			A[i][j] /= A[i][i];
+		}
+		b[i] /= A[i][i];
+		A[i][i] = 1;
 
-    for (int i = N - 1; i >= 0; i--)
-    {
-        for (int j = i + 1; j < N; j++)
-        {
-            x[i] -= A[i][j] * x[j];
-        }
+		for (int j = i + 1; j < N; j++)
+		{
+			for (int k = N - 1; k > i; k--)
+			{
+				A[j][k] = A[j][k] - A[j][i] * A[i][k];
+			}
+			b[j] = b[j] - A[j][i] * b[i];
+			A[j][i] = 0;
+		}
+	}
 
-        x[i] /= A[i][i];
-    }
+	//обратный ход
+	x[N - 1] = b[N - 1];
+	for (int i = N - 2; i > -1; i--)
+	{
+		for (int j = i + 1; j < N; j++)
+		{
+			sum += x[j] * A[i][j];
+		}
+		x[i] = b[i] - sum;
+		sum = 0;
+	}
 }
 
 
@@ -53,7 +78,18 @@ void rodkinav::lab2()
  */
 void rodkinav::lab3()
 {
-
+	double* alfa = new double[N];
+	double* beta = new double[N];
+	alfa[0] = A[0][1] / -A[0][0];
+	beta[0] = b[0] / A[0][0];
+	for (int i = 1; i < N; i++) {
+		alfa[i] = A[i][i + 1] / (-A[i][i] - A[i][i - 1] * alfa[i - 1]);
+		beta[i] = (A[i][i - 1] * beta[i - 1] - b[i]) / (-A[i][i] - A[i][i - 1] * alfa[i - 1]);
+	}
+	x[N - 1] = beta[N - 1];
+	for (int i = N - 2; i >= 0; i--) {
+		x[i] = alfa[i] * x[i + 1] + beta[i];
+	}
 }
 
 
