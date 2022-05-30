a22acb065b59e4b7abaa274e1a64365bbf1931e1
Тимовкин Антон Ю <timovkinay@lab227.local>
2022-05-30 09:59:53 -0300

    lab3

--- timovkinayu.cpp
+++ timovkinayu.cpp
@@ -49,7 +49,28 @@ void timovkinayu::lab2()
  */
 void timovkinayu::lab3()
 {
+	double* alpha = new double[N]; 
+	double* betta = new double[N]; 
 
+	alpha[0] = -A[0][1] / A[0][0];
+	betta[0] = b[0] / A[0][0];
+
+	for (int i = 1; i < N; i++) { 
+
+		alpha[i] = A[i][i + 1] / (-A[i][i] - A[i][i - 1] * alpha[i - 1]);
+		betta[i] = (-b[i] + A[i][i - 1] * betta[i - 1]) / (-A[i][i] - A[i][i - 1] * alpha[i - 1]);
+	}
+
+
+	x[N - 1] = betta[N - 1];
+	for (int i = N - 2; i >= 0; i--) 
+		x[i] = alpha[i] * x[i + 1] + betta[i];
+
+
+	for (int i = 0; i < N; i++) cout << x[i] << " ";
+	cout << endl;
+	delete[] alpha;
+	delete[] betta;
 }
 
 
