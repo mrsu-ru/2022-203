4bba3cc1faf41eb8eef4adee65f02fcd28eb4c32
D. A. Fedin <79109965+Den-Krutov@users.noreply.github.com>
2022-06-06 09:21:37 +0300

    Update fedinda.cpp

--- fedinda.cpp
+++ fedinda.cpp
@@ -163,7 +163,40 @@ void fedinda::lab5()
  */
 void fedinda::lab6()
 {
-
+    double err;
+    double Ax, Ay[N];
+    double y0[N];
+    double t, x_prev;
+    double sum1, sum2;
+    do{
+        for (int i=0; i<N; i++){
+            Ax=0;
+            for (int j=0; j<N; j++){
+                Ax+=A[i][j]*x[j];
+            }
+            y0[i]=b[i]-Ax;
+        }
+        for (int i=0; i<N; i++){
+            Ay[i]=0;
+            for (int j=0; j<N; j++){
+                Ay[i]+=A[i][j]*y0[j];
+            }
+        }
+        sum1=0; sum2=0;
+        for (int i=0; i<N; i++){
+            sum1+=y0[i]*Ay[i];
+            sum2+=Ay[i]*Ay[i];
+        }
+        t=sum1/sum2;
+        err=0;
+        for(int i=0; i<N; i++){
+            x_prev=x[i];
+            x[i]+=t*y0[i];
+            if (abs(x[i]-x_prev)>err){
+                err=abs(x[i]-x_prev);
+            }
+        }
+    }while (err>eps);
 }
 
 
@@ -173,7 +206,45 @@ void fedinda::lab6()
  */
 void fedinda::lab7()
 {
+	double *r = new double[N];
+	double *z = new double[N];
+	double *Az = new double[N];
+	double alfa, betta, tmp, norma, b_norm=0;
 
+	for(int i=0; i<N; i++){
+		b_norm += b[i]*b[i]; 
+		r[i] = b[i];
+		for(int j=0; j<N; j++)
+			r[i]-=A[i][j]*x[j];
+		z[i]=r[i];
+	}
+
+	do{
+		for(int i=0; i<N; i++){
+			Az[i]=0;
+			for(int j=0; j<N; j++)
+				Az[i]+=A[i][j]*z[j];
+		}
+		alfa=0; tmp=0;
+		for(int i=0; i<N; i++){
+			alfa+=r[i]*r[i];
+			tmp+=Az[i]*r[i];
+		}
+		alfa/=tmp;
+
+		betta=0; tmp=0;
+    	for(int i=0; i<N; i++){
+			tmp+=r[i]*r[i];
+			x[i]+=alfa*z[i];
+			r[i]-=alfa*Az[i];
+			betta+=r[i]*r[i];
+		}
+		norma=sqrt(betta/b_norm);
+		betta/=tmp;
+		
+		for(int i=0; i<N; i++)
+			z[i]=r[i]+betta*z[i];
+	}while(norma>eps);
 }
 
 
@@ -182,7 +253,68 @@ void fedinda::lab7()
  */
 void fedinda::lab8()
 {
+	double err = 0;
+	double** C = new double*[N];
 
+	for (int i = 0; i < N; i++){
+		C[i] = new double[N];
+	}
+
+
+	for (int i = 0; i < N; i++){
+		for (int j = i+1; j < N; j++){
+    			if(i!=j){ 
+      				err+=A[i][j]* A[i][j];
+   			}
+    			C[i][j] = 0;
+  		}
+	}
+
+	while (sqrt(err) > eps){
+		int mI = 0, mJ = 1;
+		for (int i = 0; i < N; i++)
+			for (int j = i+1; j<N; j++)
+				if ( abs(A[i][j]) > abs(A[mI][mJ]) ){
+  	    				mI = i; mJ = j;	
+				}
+
+	double phi;
+  	if(A[mI][mI]!= A[mJ][mJ]){
+    		phi = 0.5*atan(2*A[mI][mJ]/(A[mI][mI] - A[mJ][mJ]));
+ 	}else{phi = M_PI/4;}
+	double c = cos(phi), s = sin(phi);
+ 
+	C[mI][mI] = pow(c, 2)*A[mI][mI] - 2*s*c*A[mI][mJ] + pow(s, 2)*A[mJ][mJ];
+	C[mJ][mJ] = pow(s, 2)*A[mI][mI] + 2*s*c*A[mI][mJ] + pow(c, 2)*A[mJ][mJ];
+	C[mI][mJ] = (pow(c, 2) - pow(s, 2))*A[mI][mJ] + s*c*(A[mJ][mJ] - A[mI][mI]);
+	C[mJ][mI] = C[mI][mJ];
+	
+	for (int k = 0; k < N; k++){
+		if (k != mI  &&  k != mJ){
+	  		C[mI][k] = c*A[mI][k] - s*c*A[mJ][k];
+	  		C[k][mI] = C[mI][k];
+	  		C[mJ][k] = s*A[mI][k] + c*A[mJ][k];
+	  		C[k][mJ] = C[mJ][k];
+		} 
+		for (int l = 0; l < N; l++)
+	    		if (k != mI && k != mJ && l != mI && l != mJ) 
+				C[k][l] = A[k][l];	  
+	}
+	
+	err = 0;
+	for (int i = 0; i < N; i++)
+        	for (int j = i+1; j < N; j++)
+  	        	if (i != j) 
+			err += C[i][j] * C[i][j]; 
+
+	for (int i = 0; i < N; i++)
+        	for (int j = 0; j < N; j++) 
+	  		A[i][j] = C[i][j]; 
+
+  }
+  
+  	for (int i = 0; i < N; i++) 
+  		x[i] = A[i][i];
 }
 
 
@@ -191,7 +323,34 @@ void fedinda::lab8()
  */
 void fedinda::lab9()
 {
+	int n = N;
+	double *y = new double[n];
+	double *y_next = new double[n];
+	double lyambda = 1;
+  	double lyambda_next = 0;
 
+  	for(int i = 0; i<n; i++)
+    		y[i] = b[i];
+
+  	do{
+  		for(int i=0; i<n; i++){
+      			for(int j=0; j<n; j++){
+        			y_next[i] += A[i][j]*y[j];
+      			}
+  		}
+  		lyambda = lyambda_next;
+
+  		for(int i=0; i<n; i++){
+    			if(y[i]!= 0 && y_next[i] != 0){
+      				lyambda_next = y_next[i]/y[i];
+      				break;
+    			}
+  		}
+  		for (int i=0; i<n; i++)
+   			y[i]=y_next[i];
+	}while(fabs(lyambda_next - lyambda)>eps);
+
+	cout<<"Result: "<<lyambda_next << endl;
 }
 
 
